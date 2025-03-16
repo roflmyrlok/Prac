439a7be90978d3d6c14e994f360c3606693b1f2d
Andrii <atrybushnyi@kse.org.ua>
2025-03-16 16:46:25 +0200

    Init

---
+++ Reciever/Program.cs
@@ -0,0 +1,53 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+var port = 5010;
+var listener = new TcpListener(IPAddress.Any, port);
+listener.Start();
+Console.WriteLine("Receiver: Listening on port " + port);
+
+while (true)
+{
+	using var client = listener.AcceptTcpClient();
+	Console.WriteLine("Receiver: Connection accepted.");
+	using NetworkStream stream = client.GetStream();
+	
+	// if you want to read N (where N is small number), do
+	// var buffer = new byte[N];
+	// var bytesRead = stream.Read(buffer, 0, buffer.Length);
+    
+	// if you need to convert byte array to int, do
+	// var intValue = BitConverter.ToInt32(buffer, 0);
+    
+	// if you want to read big N, you must do it in chunks
+	// var messageBuffer = new byte[Length];
+	// var totalRead = 0;
+	//while (totalRead < Length)
+	// {
+	// int read = stream.Read(messageBuffer, totalRead, Length - totalRead);
+	// if (read == 0)
+	// {
+	// break;
+	// }
+	// totalRead += read;
+	// }
+	
+	var buffer = new byte[4];
+	var bytesRead = stream.Read(buffer, 0, buffer.Length);
+	var size = BitConverter.ToInt32(buffer, 0);
+	
+	var messageBuffer = new byte[size];
+	 var totalRead = 0;
+	while (totalRead < size)
+	 {
+	 int read = stream.Read(messageBuffer, totalRead, size - totalRead);
+	 if (read == 0)
+	 {
+	 break;
+	 }
+	 totalRead += read;
+	 }
+
+	var pos = Position.Parser.ParseFrom(messageBuffer);
+	Console.WriteLine(pos);
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Net.Sockets;
+using Google.Protobuf;
+
+var serverIp = "127.0.0.1";
+var port = 5010;
+
+try
+{
+	using var client = new TcpClient();
+	Console.WriteLine("Sender: Connecting to " + serverIp + ":" + port);
+	client.Connect(serverIp, port);
+	using var stream = client.GetStream();
+    
+	// if you need to convert int value to bytes array
+	
+
+	var pos = new Position()
+	{
+		X = 1, Y = 2, Z = 3
+	};
+	
+	var msg = pos.ToByteArray();
+	
+	byte[] lengthBytes = BitConverter.GetBytes(msg.Length);
+	
+	stream.Write(lengthBytes, 0, lengthBytes.Length);
+	
+	stream.Write(msg, 0, msg.Length);
+	
+	
+	// Or an array
+	// stream.Write(messageBytes, 0, messageBytes.Length);
+	Console.WriteLine("Sender: Message sent.");
+}
+catch (Exception ex)
+{
+	Console.WriteLine("Sender: Exception - " + ex.Message);
+}
